ffb938294b2505fb4a65986f64eabcbb3ed5af7b
Restyled.io <commits@restyled.io>
2023-09-09 08:34:58 +0000

    Restyled by prettier

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-const gold_medal =
-    `<img class="gold-medal" src="assets/img/gold-medal.png" alt=""/>`;
+const gold_medal = `<img class="gold-medal" src="assets/img/gold-medal.png" alt=""/>`;
 let api_response = [];
 let tableHTML = "";
 
 fetch(
-    "https://script.google.com/macros/s/AKfycbwNwVhQXjsohc0KyhZuqUapvNAZOupwVS3peWEVLkHyiYj_DpW4qhmKo2KleZzzomqb6Q/exec")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      api_response = data;
-      document.getElementById("leaderboard-table").innerHTML = "";
-      for (let i = 0; i < api_response.length; i++) {
-        tableHTML += `<tr>
+  "https://script.google.com/macros/s/AKfycbwNwVhQXjsohc0KyhZuqUapvNAZOupwVS3peWEVLkHyiYj_DpW4qhmKo2KleZzzomqb6Q/exec",
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    api_response = data;
+    document.getElementById("leaderboard-table").innerHTML = "";
+    for (let i = 0; i < api_response.length; i++) {
+      tableHTML += `<tr>
         <td class="number">${api_response[i].NO}</td>
         <td class="name">${api_response[i].NAME}</td>
         <td class="points">${api_response[i].SOLVED} ${
-            i === 0 ? gold_medal : ""}</td>
+          i === 0 ? gold_medal : ""
+        }</td>
         </tr>`;
-      }
-      document.getElementById("leaderboard-table").innerHTML = tableHTML;
-    })
-    .catch((error) => {
-      console.log(error);
-      document.getElementById("leaderboard-table").innerHTML =
-          `<p>Something went wrong. Please try again later.</p>`;
-    });
+    }
+    document.getElementById("leaderboard-table").innerHTML = tableHTML;
+  })
+  .catch((error) => {
+    console.log(error);
+    document.getElementById(
+      "leaderboard-table",
+    ).innerHTML = `<p>Something went wrong. Please try again later.</p>`;
+  });
 
 document.addEventListener("DOMContentLoaded", () => {
   const shareButton = document.getElementById("shareButton");
@@ -32,14 +34,19 @@ document.addEventListener("DOMContentLoaded", () => {
   shareButton.addEventListener("click", () => {
     if (navigator.share) {
       const shareData = {
-        title : "Beta-leetcoders Leaderboard",
-        text : "Check out the leaderboard for the beta-leetcoders!",
-        url : "https://blankspace-community.github.io/leetboard/",
+        title: "Beta-leetcoders Leaderboard",
+        text: "Check out the leaderboard for the beta-leetcoders!",
+        url: "https://blankspace-community.github.io/leetboard/",
       };
 
-      navigator.share(shareData)
-          .then(() => { console.log("Shared successfully!"); })
-          .catch((error) => { console.error("Error sharing:", error); });
+      navigator
+        .share(shareData)
+        .then(() => {
+          console.log("Shared successfully!");
+        })
+        .catch((error) => {
+          console.error("Error sharing:", error);
+        });
     } else {
       console.log("Share API not supported in this browser");
     }
