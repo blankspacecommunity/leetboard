637efaf4246a31c55c88c028aee1d8bdb83f82cd
Adhil Salim <bizadhil@gmail.com>
2023-09-09 15:00:55 +0530

    re-organize code

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
+// GLOBAL VARIABLES
+
 const gold_medal = `<img class="gold-medal" src="assets/img/gold-medal.png" alt=""/>`;
 let api_response = [];
 let tableHTML = "";
 
-fetch(
-  "https://script.google.com/macros/s/AKfycbwNwVhQXjsohc0KyhZuqUapvNAZOupwVS3peWEVLkHyiYj_DpW4qhmKo2KleZzzomqb6Q/exec"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    api_response = data;
-    document.getElementById("leaderboard-table").innerHTML = "";
-    for (let i = 0; i < api_response.length; i++) {
-      tableHTML += `<tr>
-        <td class="number">${api_response[i].NO}</td>
-        <td class="name">${api_response[i].NAME}</td>
-        <td class="points">${api_response[i].SOLVED} ${
-        i === 0 ? gold_medal : ""
-      }</td>
-        </tr>`;
-    }
-    document.getElementById("leaderboard-table").innerHTML = tableHTML;
-  })
-  .catch((error) => {
-    console.log(error);
-    document.getElementById(
-      "leaderboard-table"
-    ).innerHTML = `<p>Something went wrong. Please try again later.</p>`;
-  });
+let loadDetails = localStorage.getItem("pageLoadCount");
 
+// Share API
 document.addEventListener("DOMContentLoaded", () => {
   const shareButton = document.getElementById("shareButton");
 
@@ -53,11 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// remove pageLoadCount
-// localStorage.removeItem("pageLoadCount");
-
-let loadDetails = localStorage.getItem("pageLoadCount");
-
+// Page Load Count
 if (loadDetails) {
   let counter = JSON.parse(loadDetails).pageLoadCount + 1;
   localStorage.setItem(
@@ -70,12 +45,45 @@ if (loadDetails) {
     JSON.stringify({ pageLoadCount: 1, date: new Date() })
   );
 }
-loadDetails = localStorage.getItem("pageLoadCount");
-const savedDetails = JSON.parse(loadDetails);
 
-if (
-  savedDetails.pageLoadCount > 4 &&
-  new Date(savedDetails.date).toDateString() === new Date().toDateString()
-) {
-  console.log("Max limit reached");
+// Fetch API
+if (!maxLimitReached()) {
+  fetch(
+    "https://script.google.com/macros/s/AKfycbwNwVhQXjsohc0KyhZuqUapvNAZOupwVS3peWEVLkHyiYj_DpW4qhmKo2KleZzzomqb6Q/exec"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      api_response = data;
+      document.getElementById("leaderboard-table").innerHTML = "";
+      for (let i = 0; i < api_response.length; i++) {
+        tableHTML += `<tr>
+          <td class="number">${api_response[i].NO}</td>
+          <td class="name">${api_response[i].NAME}</td>
+          <td class="points">${api_response[i].SOLVED} ${
+          i === 0 ? gold_medal : ""
+        }</td>
+          </tr>`;
+      }
+      document.getElementById("leaderboard-table").innerHTML = tableHTML;
+    })
+    .catch((error) => {
+      console.log(error);
+      document.getElementById(
+        "leaderboard-table"
+      ).innerHTML = `<p>Something went wrong. Please try again later.</p>`;
+    });
+}
+
+// Max Limit Reached
+function maxLimitReached() {
+  loadDetails = localStorage.getItem("pageLoadCount");
+  const savedDetails = JSON.parse(loadDetails);
+  if (
+    savedDetails.pageLoadCount > 4 &&
+    new Date(savedDetails.date).toDateString() === new Date().toDateString()
+  ) {
+    return true;
+  }
+  return false;
 }
